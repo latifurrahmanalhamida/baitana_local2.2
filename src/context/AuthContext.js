8a159cdbe9f91e87412a09ee41d769dc293662d0
rafioosadani <rafioosadani@gmail.com>
2025-06-12 04:20:03 +0700

    chore: optimize base URL usage by updating local .env configuration

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -6,7 +6,7 @@ import apiClient from '@/lib/apiClient';
 import { toast } from "sonner"
 
 const AuthContext = createContext();
-const BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000/api';
+const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:8000/api';
 
 export const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
--- src/lib/apiClient.js
+++ src/lib/apiClient.js
@@ -1,6 +1,6 @@
 import Cookies from "js-cookie";
 
-const BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000/api";
+const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:8000/api";
 
 class ApiClient {
     constructor() {
