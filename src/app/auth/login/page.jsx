4ba875051464d2e33525857ff95c3b4634765951
rafioosadani <rafioosadani@gmail.com>
2025-06-11 13:06:48 +0700

    feat: enhance login page background with layered gradients and patterns

--- src/app/auth/login/page.jsx
+++ src/app/auth/login/page.jsx
@@ -3,7 +3,26 @@ import { LoginForm } from "@/components/login/login-form";
 export default function LoginPage() {
     return (
         <div
-            className="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
+            className="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10"
+            style={{
+                backgroundImage: `
+                    linear-gradient(to right, rgba(0, 0, 0, 0.03) 1px, transparent 1px), /* Pola Vertikal (Lapisan Atas) */
+                    linear-gradient(to bottom, rgba(0, 0, 0, 0.03) 1px, transparent 1px), /* Pola Horizontal (Lapisan Atas) */
+                    linear-gradient(to left, #e0e7ff, var(--muted))
+                `,
+                backgroundSize: `
+                    30px 30px,    
+                    30px 30px,    
+                    100% 100%     
+                `,
+                backgroundRepeat: `
+                    repeat,      
+                    repeat,      
+                    no-repeat     
+                `,
+                backgroundColor: 'var(--muted)',
+            }}
+        >
             <div className="w-full max-w-sm md:max-w-3xl">
                 <LoginForm title={"Selamat Datang Kembali!"} description={"Silahkan login untuk masuk ke sistem."} />
             </div>
