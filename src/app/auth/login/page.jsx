a0c99208ce1f68d2594a95355c31d034a67af30f
rafioosadani <rafioosadani@gmail.com>
2025-06-11 13:04:04 +0700

    feat: enhance registration page background with layered gradients and patterns

--- src/app/auth/login/page.jsx
+++ src/app/auth/login/page.jsx
@@ -1,30 +1,11 @@
-import { RegisterForm } from "@/components/register/register-form";
+import { LoginForm } from "@/components/login/login-form";
 
-export default function RegisterPage() {
+export default function LoginPage() {
     return (
         <div
-            className="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10"
-            style={{
-                backgroundImage: `
-                    linear-gradient(to right, rgba(0, 0, 0, 0.03) 1px, transparent 1px), /* Pola Vertikal (Lapisan Atas) */
-                    linear-gradient(to bottom, rgba(0, 0, 0, 0.03) 1px, transparent 1px), /* Pola Horizontal (Lapisan Atas) */
-                    linear-gradient(to right, #e0e7ff, var(--muted))
-                `,
-                backgroundSize: `
-                    30px 30px,    
-                    30px 30px,    
-                    100% 100%     
-                `,
-                backgroundRepeat: `
-                    repeat,      
-                    repeat,      
-                    no-repeat     
-                `,
-                backgroundColor: 'var(--muted)',
-            }}
-        >
-            <div className="flex w-full max-w-sm md:max-w-5xl justify-center">
-                <RegisterForm title={"Selamat Datang!"} description={"Lengkapi form ini untuk membuat akun baru."} />
+            className="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
+            <div className="w-full max-w-sm md:max-w-3xl">
+                <LoginForm title={"Selamat Datang Kembali!"} description={"Silahkan login untuk masuk ke sistem."} />
             </div>
         </div>
     );
--- src/app/auth/register/page.jsx
+++ src/app/auth/register/page.jsx
@@ -1,12 +1,31 @@
-import { RegisterForm } from "@/components/register/register-form"
+import { RegisterForm } from "@/components/register/register-form";
 
 export default function RegisterPage() {
-  return (
-    <div
-      className="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
-      <div className="flex w-full max-w-sm md:max-w-5xl justify-center">
-          <RegisterForm title={"Selamat Datang!"} description={"Lengkapi form ini untuk membuat akun baru."} />
-      </div>
-    </div>
-  );
+    return (
+        <div
+            className="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10"
+            style={{
+                backgroundImage: `
+                    linear-gradient(to right, rgba(0, 0, 0, 0.03) 1px, transparent 1px), /* Pola Vertikal (Lapisan Atas) */
+                    linear-gradient(to bottom, rgba(0, 0, 0, 0.03) 1px, transparent 1px), /* Pola Horizontal (Lapisan Atas) */
+                    linear-gradient(to right, #e0e7ff, var(--muted))
+                `,
+                backgroundSize: `
+                    30px 30px,    
+                    30px 30px,    
+                    100% 100%     
+                `,
+                backgroundRepeat: `
+                    repeat,      
+                    repeat,      
+                    no-repeat     
+                `,
+                backgroundColor: 'var(--muted)',
+            }}
+        >
+            <div className="flex w-full max-w-sm md:max-w-5xl justify-center">
+                <RegisterForm title={"Selamat Datang!"} description={"Lengkapi form ini untuk membuat akun baru."} />
+            </div>
+        </div>
+    );
 }
